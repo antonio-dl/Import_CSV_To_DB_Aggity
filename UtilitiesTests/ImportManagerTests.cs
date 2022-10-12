a34dac81f8c6c5390733952c6ed9cd3cb56c4183
antonio.deluca <antonio.deluca96@gmail.com>
2022-10-13 01:01:21 +0200

    test suite started

---
+++ UtilitiesTests/ImportManagerTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities.Tests
+{
+    [TestClass()]
+    public class ImportManagerTests
+    {
+        ImportManager importManager = new( "production.brands", @"Data Source=localhost;Initial Catalog=Bike;Integrated Security=True");
+
+        [TestMethod()]
+        public void ImportCSVToDatabaseTest()
+        {
+            importManager.ImportCSVToDatabase(@"C:\Users\antonio.deluca\Desktop\bike-production-brands.txt", ',');
+
+
+        }
+    }
+}
